b001724eb6f6891e08399543fc12341fb96252e4
rongw4 <rongw4@illinois.edu>
2021-11-05 22:07:23 -0500

    initial Mysql

--- OOTD/settings.py
+++ OOTD/settings.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__, instance_relative_config=True)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'project'
+
+# init MySQL
+mysql = MySQL(app)
