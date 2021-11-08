8b6a0cbaf7eb10b3331c0bb0ed500667acd82b2e
rongwang <rongw4@illinois.edu>
2021-11-08 17:40:51 -0600

    add 2 advanced query, return result

--- OOTD/views/database.py
+++ OOTD/views/database.py
@@ -5,19 +5,18 @@ from OOTD.settings import db
 # search product, return id
 def product_search(product_name: str):
     conn = db.connect()
-    query = "select id from product where name = '{}';".format(product_name)
+    query = "select * from product where name like '%%{}%%';".format(product_name)
     result = conn.execute(query)
     conn.close()
-    id = result.first()[0]
-    print(id)
-    return id
+    # result is a cursor result object
+    return result
 
 
 # add category
 def add_category(gender: str, master_cate: str, sub_cate: str, article: str):
     conn = db.connect()
     query = 'insert into category values ("{}","{}","{}","{}");'.format(
-        gender, master_cate,sub_cate, article)
+        gender, master_cate, sub_cate, article)
     conn.execute(query)
     conn.close()
 
@@ -45,3 +44,76 @@ def del_outfit(outfit_id):
     query = 'delete from outfits where id = "{}";'.format(outfit_id)
     conn.execute(query)
     conn.close()
+
+
+# count user name
+def cnt_uname():
+    conn = db.connect()
+    query = 'select count(*) from user'
+    result = conn.execute(query)
+    conn.close()
+    cnt = result.first()[0]
+    print(cnt)
+    return cnt
+
+
+# count product
+def cnt_product():
+    conn = db.connect()
+    query = 'select count(*) from product'
+    result = conn.execute(query)
+    conn.close()
+    cnt = result.first()[0]
+    print(cnt)
+    return cnt
+
+
+# count user name
+def cnt_category():
+    conn = db.connect()
+    query = 'select count(*) from category'
+    result = conn.execute(query)
+    conn.close()
+    cnt = result.first()[0]
+    print(cnt)
+    return cnt
+
+
+# count user name
+def cnt_outfits():
+    conn = db.connect()
+    query = 'select count(*) from outfits'
+    result = conn.execute(query)
+    conn.close()
+    cnt = result.first()[0]
+    print(cnt)
+    return cnt
+
+
+# advanced query
+def adv1():
+    conn = db.connect()
+    query = 'SELECT u.user_id, u.name, b.cnt ' \
+            'FROM user u JOIN (SELECT author_id, COUNT(id) AS cnt FROM blog GROUP BY author_id) AS b ' \
+            'ON user_id = author_id ' \
+            'ORDER BY b.cnt DESC LIMIT 15;'
+    result = conn.execute(query)
+    conn.close()
+    for row in result:
+        print(row)
+    return result
+
+
+def adv2():
+    conn = db.connect()
+    query = "(SELECT u.name, u.email, b.blog_text " \
+            "FROM user u JOIN (SELECT author_id, blog_text FROM blog WHERE blog_text LIKE '%%{}%%' ) AS b " \
+            "ON author_id = user_id WHERE u.gender = 'MALE' ORDER BY u.user_id) " \
+            "UNION " \
+            "(SELECT u.name, u.email, b.blog_text " \
+            "FROM user u JOIN (SELECT author_id, blog_text FROM blog WHERE blog_text LIKE '%%{}%%' ) AS b " \
+            "ON author_id = user_id WHERE u.gender = 'MALE' ORDER BY u.user_id)".format('tutu', 'disney')
+    result = conn.execute(query)
+    conn.close()
+    for row in result:
+        print(row)
