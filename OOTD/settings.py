98a242ca2f59947e33139f246d1d6bb30ab4748d
MarcoYLyu <myilyu19990707@gmail.com>
2021-11-26 16:03:02 -0600

    added google cloud

--- OOTD/settings.py
+++ OOTD/settings.py
@@ -7,12 +7,26 @@ app.secret_key = 'cs411'
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 
+app.config.from_pyfile("ootd.cfg")
 
 # init MySQL
 def init_connection_engine():
-    pool = sqlalchemy.create_engine('mysql://root:123456@localhost/project')
+    host = app.config["HOST"]
+    driver_name = app.config["DRIVER_NAME"]
+    username = app.config["USERNAME"]
+    password = app.config["PASSWORD"]
+    database = app.config["DATABASE"]
+    ssl_args = app.config["SSL_ARGS"]
+
+    sql_url = sqlalchemy.engine.URL(
+        host=host,
+        drivername=driver_name,
+        username=username,
+        password=password,
+        database=database
+    )
+    pool = sqlalchemy.create_engine(sql_url, connect_args=ssl_args)
     return pool
 
 
 db = init_connection_engine()
-
